8b5985568372d694794a5045968f3a1acd33c400
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2025-01-24 17:18:16 +0100

    update stacks

--- stack-compute-aws/terraform/module-compute/instance.tf
+++ stack-compute-aws/terraform/module-compute/instance.tf
@@ -43,7 +43,7 @@ resource "aws_security_group_rule" "ingress-http" {
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.debian.id
   instance_type = var.vm_instance_type
-  key_name      = var.key_pair_name
+  # key_name      = var.key_pair_name
 
   vpc_security_group_ids = [aws_security_group.ec2.id]
 
