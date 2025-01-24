198881bd701c2b2291f8714e52fda7de347f0d44
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2025-01-24 16:32:52 +0100

    update stacks

--- stack-network-aws/terraform/key_pair/variables.tf
+++ stack-network-aws/terraform/key_pair/variables.tf
@@ -1,16 +1,17 @@
 # Cycloid
+variable "component" {}
 variable "customer" {}
 variable "env" {}
 variable "project" {}
 
 variable "key_pair_name" {
   description = "Public key to provision to the instance."
-  default = ""
+  default     = ""
 }
 
 variable "key_pair_public" {
   description = "Public key to create."
-  default = ""
+  default     = ""
 }
 
 # Tags
@@ -21,9 +22,10 @@ variable "extra_tags" {
 locals {
   standard_tags = {
     "cycloid" = "true"
-    env          = var.env
-    project      = var.project
-    customer     = var.customer
+    env       = var.env
+    project   = var.project
+    customer  = var.customer
   }
   merged_tags = merge(local.standard_tags, var.extra_tags)
 }
+
