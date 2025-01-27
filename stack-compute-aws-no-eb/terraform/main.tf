910e6039687bcfbcd43a1b775757b75fc26742e8
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2025-01-27 14:14:16 +0100

    update stacks

---
+++ stack-compute-aws-no-eb/terraform/main.tf
@@ -0,0 +1,37 @@
+module "compute" {
+  #####################################
+  # Do not modify the following lines #
+  source    = "./module-compute"
+  project   = var.project
+  env       = var.env
+  customer  = var.customer
+  component = var.component
+  #####################################
+
+  #. extra_tags (optional): {}
+  #+ Dict of extra tags to add on resources. format { "foo" = "bar" }.
+  extra_tags = {
+    demo                 = true
+    monitoring_discovery = false
+  }
+
+  #
+  # Instance
+  #
+
+  #. vm_instance_type: 't3.micro'
+  #+ Instance type for the VM
+  vm_instance_type = "Value injected by StackForms"
+
+  #. vm_disk_size: 20
+  #+ Disk size for the VM (Go)
+  vm_disk_size = "Value injected by StackForms"
+
+  #. vpc_id: ''
+  #+ VPC ID where to deploy the EC2 instance
+  vpc_id = "Value injected by StackForms"
+
+  #. key_pair_name: ''
+  #+ Public Key pair name to provision to the EC2 instance
+  # key_pair_name = "Value injected by StackForms"
+}
---
+++ stack-network-aws-no-eb/terraform/main.tf
@@ -0,0 +1,60 @@
+module "network" {
+  #####################################
+  # Do not modify the following lines #
+  source    = "./network"
+  component = var.component
+  project   = var.project
+  env       = var.env
+  customer  = var.customer
+  #####################################
+
+  #. extra_tags (optional): {}
+  #+ Dict of extra tags to add on resources. format { "foo" = "bar" }.
+  extra_tags = {
+    demo                 = true
+    monitoring_discovery = false
+  }
+
+  #. vpc_cidr: "10.0.0.0/16"
+  #+ Public Subnet CIDR
+  vpc_cidr = "10.0.0.0/16"
+
+  #. vpc_public_subnet: "10.0.0.0/24"
+  #+ Public Subnet CIDR
+  vpc_public_subnet = "10.0.0.0/24"
+
+  #. vpc_private_subnet: "10.0.1.0/24"
+  #+ Private Subnet CIDR
+  vpc_private_subnet = "10.0.1.0/24"
+
+  #. nat_gateway: false
+  #+ Whether to deploy a NAT gateway or not
+  nat_gateway = false
+}
+
+# module "key_pair" {
+#   #####################################
+#   # Do not modify the following lines #
+#   source    = "./key_pair"
+#   component = var.component
+#   project   = var.project
+#   env       = var.env
+#   customer  = var.customer
+#   #####################################
+# 
+#   #. extra_tags (optional): {}
+#   #+ Dict of extra tags to add on resources. format { "foo" = "bar" }.
+#   extra_tags = {
+#     demo                 = true
+#     monitoring_discovery = false
+#   }
+# 
+#   #. key_pair_name: ""
+#   #+ The name of the key pair to provision to the instance
+#   key_pair_name = "${var.customer}-${var.project}-${var.env}-${var.component}"
+# 
+#   #. key_pair_public: ""
+#   #+ Public key to create
+#   key_pair_public = "Value injected by StackForms"
+# }
+
