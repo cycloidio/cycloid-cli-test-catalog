ad5a4539645993ec814714afd4ea1af6f591e25d
Asier Gilaber <asier.gilaber@cycloid.io>
2025-10-08 13:03:23 +0000

    config(cli-tests): update fbh/hello/dasdasd with seraf:stack-compute@aws-ec2 by asier_gilaber

--- cli-tests/fbh/hello/dasdasd/terraform/vm.tf
+++ cli-tests/fbh/hello/dasdasd/terraform/vm.tf
@@ -77,9 +77,9 @@ module "vm" {
     }
   ]
   sg_name      = ""
-  subnet_id    = "dsadad"
+  subnet_id    = "dasdsa"
   vm_disk_size = 20
   vm_disk_type = "gp2"
-  vpc_id       = "dasdasdadas"
+  vpc_id       = "dsadsa"
 }
 
